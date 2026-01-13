0f3eecd2e49abd2a0593b774dc1fb3d2a7277c25
Matthew Murno <murno@bc.edu>
2026-01-12 20:06:45 -0500

    init: adding the full rag process

---
+++ app/services/rag_system.py
@@ -0,0 +1,42 @@
+from typing import Dict
+from app.core.config import Config
+from app.services.bedrockllm import BedrockLLM
+from app.services.embedding_manager import EmbeddingManager
+from app.services.chroma_manager import ChromaDBManager
+
+class RAGSystem:
+    def __init__(self):
+        self.embedding_manager = EmbeddingManager()
+        self.chroma_manager = ChromaDBManager()
+        self.bedrock_llm = BedrockLLM()
+
+    def query(self, query, top_k: int = Config.TOP_K_ChUNKS) -> Dict:
+        query_embedding = self.embedding_manager.get_embedding(query)
+        results = self.chroma_manager.similarity_search_by_vector(query_embedding, k=top_k)
+
+        retrieved_chunks = []
+        for i in range(len(results["ids"][0])):
+            retrieved_chunks.append(
+                {
+                    "id": results["ids"][0][i],
+                    "text": results["documents"][0][i],
+                    "metadata": results["metadata"][0][i],
+                    "distance": results.get("distances")[0][i]
+                    if "distances" in results
+                    else None,
+                }
+            )
+
+        context = "\n\n".join(
+            f"[Section: {chunk['metadata'].get('section', 'unknown')}]\n{chunk['text']}"
+            for chunk in retrieved_chunks
+        )
+
+        answer = self.bedrock_llm.generate_response(query, context)
+
+        return {
+            "query": query,
+            "answer": answer,
+            "retrieved_chunks": retrieved_chunks,
+            "num_chunks": len(retrieved_chunks),
+        }
